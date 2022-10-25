169e774aeabfc45661e78e9055826a2f6c42f771
Haein Kim <hayni100@gmail.com>
2022-10-24 23:01:07 -0700

    added eventListener click to each choices

--- assets/script.js
+++ assets/script.js
@@ -32,60 +32,60 @@ var timeLeft = 100;
 //////objects for each question and choices//////
 var questions = [
   {
-  title: "1. Which of the following best describes a Web API?",
-  choices: ["Web APIs are low level code (say C or C++) that directly control the computer's GPU or other graphics functions.",
+    title: "1. Which of the following best describes a Web API?",
+    choices: ["Web APIs are low level code (say C or C++) that directly control the computer's GPU or other graphics functions.",
 
-    "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web.",
-  
-    "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.",
-  
-    "Web APIs are a part of the JavaScript language itself and are built into your browser."],
-  answer: "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web."
-},
+      "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web.",
 
-{
-  title: "2. Your colleague notices that when she clicks on a <p> element on her page, handlers run on <p> and on <p>'s parent elements as well. She asks you to help her debug. Which of the following is most likely?",
-  
-  choices: ["The parent node of <p> is triggering a bubbling event that is bubbling down towards <p> when it is clicked.", 
-  "She forgot to add event.preventDefault() in her script.js file.",
+      "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.",
 
-  "A bubbling event is occurring that starts with the target element, <p>, and is then running handlers on <p>'s parent and other ancestors.",
+      "Web APIs are a part of the JavaScript language itself and are built into your browser."],
+    answer: "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web."
+  },
 
-  "She added an event listener in the wrong place in her html file."
-  ],
-  
-  answer: "A bubbling event is occurring that starts with the target element, <p>, and is then running handlers on <p>'s parent and other ancestors."
-},
+  {
+    title: "2. Your colleague notices that when she clicks on a <p> element on her page, handlers run on <p> and on <p>'s parent elements as well. She asks you to help her debug. Which of the following is most likely?",
 
-{
-  title:  "3. Which property can you use in order to implement event delegation?",
+    choices: ["The parent node of <p> is triggering a bubbling event that is bubbling down towards <p> when it is clicked.",
+      "She forgot to add event.preventDefault() in her script.js file.",
 
-  choices: ["event.preventDefault()",
+      "A bubbling event is occurring that starts with the target element, <p>, and is then running handlers on <p>'s parent and other ancestors.",
 
-  "event.stopPropagation()",
-   
-  "event.addEventListener()",
-  
-  "event.target"],
+      "She added an event listener in the wrong place in her html file."
+    ],
 
-  answer: "event.target"
-},
+    answer: "A bubbling event is occurring that starts with the target element, <p>, and is then running handlers on <p>'s parent and other ancestors."
+  },
 
-{
-  title: "4.Which statement best describes what is happening to data when it is persisted to local storage?",
+  {
+    title: "3. Which property can you use in order to implement event delegation?",
 
-  choices: ["The data is stored in the client or browser.",
+    choices: ["event.preventDefault()",
 
-  "The data is stored in the window called localStorage.",
+      "event.stopPropagation()",
 
-  "The data is stored under the Applications tab in Chrome Dev Tools.",
-   
-  "The data is stored in the database in the backend."
+      "event.addEventListener()",
 
-  ],
+      "event.target"],
 
-  answer: "The data is stored in the client or browser."
-}
+    answer: "event.target"
+  },
+
+  {
+    title: "4.Which statement best describes what is happening to data when it is persisted to local storage?",
+
+    choices: ["The data is stored in the client or browser.",
+
+      "The data is stored in the window called localStorage.",
+
+      "The data is stored under the Applications tab in Chrome Dev Tools.",
+
+      "The data is stored in the database in the backend."
+
+    ],
+
+    answer: "The data is stored in the client or browser."
+  }
 ]
 
 
@@ -95,15 +95,16 @@ function mainPage() {
   timer.textContent = "Time Left: " + timeLeft;
   mainPrompt.textContent = "Code Quiz Challenge";
   mainButtons.textContent = "Start Quiz";
-quizStart();
+  quizStart();
 }
 mainPage();
 
 //Quiz-page//
 function quizStart() {
   //TIMER//
+
   mainButtons.addEventListener("click", function () {
- quizPage();
+    quizPage();
     var timerInterval = setInterval(function () {
 
       var countDown = timeLeft--;
@@ -112,63 +113,74 @@ function quizStart() {
         clearInterval(timerInterval);
         mainPrompt.textContent = "You Failed!";
         mainPrompt.style.fontSize = "200px";
-        
+
 
       }
     }, 1000);
   });
 
-  //CREATE FUNCTION that subtracts time if wrong anwer is clicked//
-  function takeOffTime () {
-
-  }
-
   //hide start quiz button, prompt questions and choices//
-function quizPage () {
-  if (mainButtons.style.display === "none") {
-    mainButtons.style.display = "block";
-  } else {
-    mainButtons.style.display = "none";
-    getQuestion(currentIndex);
+  function quizPage() {
+    if (mainButtons.style.display === "none") {
+      mainButtons.style.display = "block";
+    } else {
+      mainButtons.style.display = "none";
+      getQuestion(currentIndex);
+    }
+
+    //For loop to prompt questions in order//
+    function getQuestion(index) {
+      console.log("hello", index)
+      // get current question object from array
+      var currentQuestion = questions[index];
+      // update title with current question
+      var titleEl = document.getElementById("main-prompt");
+      titleEl.textContent = currentQuestion.title;
+      // clear out any old question choices
+      // mainPrompt.innerHTML = "";
+      // loop over choices
+      for (var i = 0; i < currentQuestion.choices.length; i++) {
+        // create new button for each choice
+        var choice = currentQuestion.choices[i];
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+        // choiceNode.textContent = i + 1 + '. ' + choice;
+        choiceNode.textContent = choice;
+
+        choiceNode.addEventListener("click", function (e) {
+
+          e.preventDefault();
+          console.log(currentIndex);
+          //increment current index and pass into getQuestion(index)//
+          currentIndex += 1;
+          getQuestion(currentIndex);
+          console.log(currentIndex);
+
+          //10 seconds deducted when choices other than answer is clicked//
+          // function takeOffTime() {
+          //   var wrongAnswer = currentQuestion.answer === false;
+          //   console.log(wrongAnswer);
+          //   wrongAnswer.addEventListener("click", function (ev) {
+          //     ev.preventDefault();
+          //     timeLeft - 10;
+          //   });
+          // }
+          
+          // takeOffTime()
+
+          console.log(currentQuestion.answer);
+
+        });
+        // display on the page
+        mainPrompt.appendChild(choiceNode);
+
+
+      }
+    }
+
   }
 
-//create for loop?//
-function getQuestion (index) {
-console.log("hello",index)
-  // get current question object from array
-  var currentQuestion = questions[index];
-  // update title with current question
-  var titleEl = document.getElementById("main-prompt");
-  titleEl.textContent = currentQuestion.title;
-  // clear out any old question choices
-  // mainPrompt.innerHTML = "";
-  // loop over choices
-  for (var i = 0; i < currentQuestion.choices.length; i++) {
-    // create new button for each choice
-    var choice = currentQuestion.choices[i];
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
-    // choiceNode.textContent = i + 1 + '. ' + choice;
-    choiceNode.textContent = choice;
-    // display on the page
-    mainPrompt.appendChild(choiceNode);
-  }
 }
-
-var indexIncrementerBtn = document.querySelector("#index-incrementer");
-//allows to change questions//
-indexIncrementerBtn.addEventListener("click", function(e){
-  e.preventDefault();
-  console.log(currentIndex);
-  //increment current index and pass into getQuestion(index)//
-  currentIndex += 1;
-  getQuestion(currentIndex);
-  console.log(currentIndex);
-});
-
-
-}
-}
-
+//FUNCTION that subtracts time if wrong anwer is clicked//
 
